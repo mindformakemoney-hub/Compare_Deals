06967eb12a97cf29254f2fa472f7be389e07fe82
mindformakemoney-hub <mindformakemoney@gmail.com>
2025-08-25 23:45:37 +0100

    app.py

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ def index():
     return render_template('index.html', products=products)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
